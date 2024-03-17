6d1063d7a1135838ed7d212f9a43d3f0c3edc790
Guillermo Pages <mail@guillermo.at>
2024-03-17 16:22:27 +0100

    feat: export multiple req loader

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import mysqlReqLoader from "./loaders/mysqlReq";
+import mysqlMultipleReqLoader from "./loaders/mysqlMultipleReq";
 import { undumpSchema, schemaFilePath } from "./data/schema";
 
 export {
   mysqlReqLoader,
+  mysqlMultipleReqLoader,
   undumpSchema,
   schemaFilePath,
 }
