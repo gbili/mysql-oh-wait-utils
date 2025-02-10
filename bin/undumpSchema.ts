9bdb0d0823c66af7f91dd0db2cdbae5a451fb583
Guillermo Pages <mail@guillermo.at>
2025-02-10 11:31:28 +0100

    fix: dependencies need updating

--- bin/undumpSchema.ts
+++ bin/undumpSchema.ts
@@ -1,56 +1,59 @@
 #!/usr/bin/env node
 import 'dotenv/config';
 import { readFileSync, existsSync } from 'fs';
-import Logger from 'saylo';
 import DiContainer from 'di-why';
 import mysql from 'mysql';
 import { MysqlReq, MysqlDump } from 'mysql-oh-wait';
-import { getUserRootDirOrThrow } from 'swiss-army-knifey'
+import { getUserRootDirOrThrow } from 'swiss-army-knifey/build/src/node'
+import { logger } from 'swiss-army-knifey';
 
 const udir = getUserRootDirOrThrow();
 
 const schemaFilePath = `${udir}/src/data/schema.sql`;
 
-const logger = new Logger({log: true, debug: true});
-
-  const envVarNames = {
-    host: 'DB_HOST',
-    user: 'DB_USER',
-    password: 'DB_PASSWORD',
-    database: 'DB_NAME',
-  };
-  const di = new DiContainer({
-    load: {
-      'mysqlReq': {
-        constructible: MysqlReq,
-        deps: {
-          adapter: mysql,
-          logger,
-          connectionConfig: {
-            ...MysqlReq.extractConfigFromEnv(process.env, envVarNames),
-            multipleStatements: true,
-          },
-        },
-        async after({me, serviceLocator}) {
-          await me.connect();
+const envVarNames = {
+  host: 'DB_HOST',
+  user: 'DB_USER',
+  password: 'DB_PASSWORD',
+  database: 'DB_NAME',
+};
+const di = new DiContainer({
+  load: {
+    'logger': {
+      instance: logger,
+    },
+    'mysqlReq': {
+      constructible: MysqlReq,
+      deps: {
+        adapter: mysql,
+        connectionConfig: {
+          ...MysqlReq.extractConfigFromEnv(process.env, envVarNames),
+          multipleStatements: true,
         },
       },
-
-      'mysqlDump': {
-        injectable: new MysqlDump(),
-        deps: {
-          logger,
-          readFileSync,
-          existsSync
-        },
-        locateDeps: {
-          requestor: 'mysqlReq'
-        },
+      locateDeps: {
+        logger: 'logger',
+      },
+      async after({me, serviceLocator}) {
+        await me.connect();
       },
     },
-  });
 
-  (async function () {
-    await di.loadAll();
-    await (await di.get('mysqlDump')).executeSqlFileOnExistingConnection({filePath: schemaFilePath, disconnectOnFinish: true});
-  })();
+    'mysqlDump': {
+      injectable: new MysqlDump(),
+      deps: {
+        readFileSync,
+        existsSync
+      },
+      locateDeps: {
+        requestor: 'mysqlReq',
+        logger: 'logger',
+      },
+    },
+  },
+});
+
+(async function () {
+  await di.loadAll();
+  await (await di.get('mysqlDump')).executeSqlFileOnExistingConnection({filePath: schemaFilePath, disconnectOnFinish: true});
+})();
--- src/data/schema.ts
+++ src/data/schema.ts
@@ -1,9 +1,8 @@
 import { MysqlDump } from 'mysql-oh-wait';
 import { GetInstanceType } from 'di-why/build/src/DiContainer';
-import { getUserRootDirOrThrow } from 'swiss-army-knifey'
+import { getUserRootDirOrThrow } from 'swiss-army-knifey/build/src/node'
+import { logger } from 'swiss-army-knifey';
 
-import Logger from 'saylo';
-const logger = new Logger({log: true, debug: true});
 const udir = getUserRootDirOrThrow();
 
 const schemaFilePath = `${udir}/src/data/schema.sql`;
